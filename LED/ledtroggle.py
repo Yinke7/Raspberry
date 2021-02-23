297af039003e2708c476522c76cb6527f30d18ae
Yinke7 <1583827745@qq.com>
2021-02-23 16:49:55 +0800

    update

--- LED/ledtroggle.py
+++ LED/ledtroggle.py
@@ -3,22 +3,23 @@
 
 import RPi.GPIO as GPIO
 import time
-import sys
 
 LED_PIN = 4
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
+
 while True:
     try:
         GPIO.output(LED_PIN, not GPIO.input(LED_PIN))
         time.sleep(1)
         print("LED_PIN Level: %s" % GPIO.input(LED_PIN))
-    except KeyboardInterrupt as err:
-        print(err)
+    except BaseException as e:
+        print(e)
         break
 
 print("GPIO.cleanup")
 GPIO.cleanup()
+
 
 
