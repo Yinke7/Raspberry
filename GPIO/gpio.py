eabebcab1d967a6382154cf4784e37f3ba7e39a0
Yinke7 <1583827745@qq.com>
2021-02-23 23:03:11 +0800

    update

--- GPIO/gpio.py
+++ GPIO/gpio.py
@@ -37,19 +37,20 @@ import RPi.GPIO as GPIO
 import time
 import threading
 
-LED_PIN = 17
+LED1_PIN = 4
+LED2_PIN = 17
 IRQ_PIN = 18
 
-def Toggle():
+def Toggle(pin):
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(LED_PIN, GPIO.OUT)
+    GPIO.setup(pin, GPIO.OUT)
 
     while True:
         try:
-            GPIO.output(LED_PIN, not GPIO.input(LED_PIN))
+            GPIO.output(pin, not GPIO.input(pin))
             time.sleep(1)
-            print("LED_PIN Level: %s" % GPIO.input(LED_PIN))
+            print("BCM%d Level: %s" % (int(pin), GPIO.input(pin)))
         except:
             break
 
@@ -60,12 +61,9 @@ def Toggle():
 def IRQ(pin):
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pin, GPIO.IN, GPIO.PUD_UP)
-
-    print("irq test")
+    GPIO.setup(IRQ_PIN, GPIO.IN, GPIO.PUD_UP)
     try:
-        GPIO.add_event_detect(pin, GPIO.FALLING, Toggle, 200)
-        print("Interrupt")
+        GPIO.add_event_detect(pin, GPIO.FALLING, Toggle(LED1_PIN), 200)
     except BaseException as exp:
         print(exp)
 
@@ -75,22 +73,17 @@ def IRQ(pin):
 
 if __name__ == "__main__":
     try:
+        thread = []
+        t1 = threading.Thread(target=Toggle, args=(LED2_PIN, ), name="led toggle")
+        thread.append(t1)
+        t2 = threading.Thread(target=IRQ, args=(IRQ_PIN, ), name="irq")
+        thread.append(t2)
 
-        t1 = threading.Thread(target=Toggle, name="led toggle")
-        t2 = threading.Thread(target=IRQ, args=(IRQ_PIN,), name="irq")
-
-        t1.start()
-        t2.start()
-
-        t1.join()
-        t2.join()
+        for t in thread:
+            t.setDaemon(True)
+            t.start()
+        for t in thread:
+            t.join()
 
     except BaseException as exp:
         print(exp)
-
-    while True:
-        pass
-
-
-
-
